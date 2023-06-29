a1321482b780c712580f1f826f407cd7525e1826
Michael Bestas <mkbestas@gmail.com>
2023-06-29 19:20:50 +0300

    config: Allow overriding ro.support_one_handed_mode
    Pixel Fold disables this
    
    Change-Id: Ia5a369d75a356800ea8792fe290094ec99b4ad64

--- config/common_full_phone.mk
+++ config/common_full_phone.mk
@@ -10,6 +10,6 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/voltage/overlay/dictionaries
 
 # Enable support of one-handed mode
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.support_one_handed_mode=true
+    ro.support_one_handed_mode?=true
 
 $(call inherit-product, vendor/voltage/config/telephony.mk)
